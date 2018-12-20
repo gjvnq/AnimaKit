799c4903e08d6734d9cacb1a5b1043440068a399
G Queiroz <gabrieljvnq@gmail.com>
2018-12-20 03:44:07 -0200

    Added hack to use ffmpeg via CLI to generate final video file.

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/gjvnq/AnimaKit"
 	"github.com/integrii/flaggy"
 )
 
-var ArgsInput string = ""
-var ArgsOutput string = ""
-var ArgsNWorkers int = 8
+var ArgInput string = ""
+var ArgOutputDir string = ""
+var ArgOutputFile string = ""
+var ArgFfmpeg string = "ffmpeg"
+var ArgNWorkers int = 8
 
 func init() {
 	// Set your program's name and description.  These appear in help output.
@@ -17,9 +21,11 @@ func init() {
 	flaggy.SetDescription("A simple animation renderer")
 	flaggy.DefaultParser.ShowHelpOnUnexpected = false
 	flaggy.DefaultParser.AdditionalHelpPrepend = "https://github.com/gjvnq/AnimaKit"
-	flaggy.Int(&ArgsNWorkers, "n", "n-workers", "Number of parallel rendering workers")
-	flaggy.AddPositionalValue(&ArgsInput, "input", 1, true, "Animation script file (.js)")
-	flaggy.AddPositionalValue(&ArgsOutput, "output", 2, false, "Directory to store the output video frames")
+	flaggy.Int(&ArgNWorkers, "n", "n-workers", "Number of parallel rendering workers")
+	flaggy.String(&ArgFfmpeg, "", "ffmpeg", "Path to ffmpeg CLI tool")
+	flaggy.AddPositionalValue(&ArgInput, "input", 1, true, "Animation script file (.js)")
+	flaggy.AddPositionalValue(&ArgOutputDir, "frames-dir", 2, false, "Directory to store the output video frames")
+	flaggy.AddPositionalValue(&ArgOutputFile, "video-output", 3, false, "Output video file (requires ffmpeg CLI tool)")
 	flaggy.Parse()
 }
 
@@ -29,16 +35,39 @@ func main() {
 	defer AnimaKit.Quit()
 
 	// Load animation
-	_, err := AnimaKit.LoadScriptFromFile(ArgsInput)
+	_, err := AnimaKit.LoadScriptFromFile(ArgInput)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
 	// fmt.Println("Use [←] and [→] to move frame by frame and [SPACE] to play")
-	if ArgsOutput == "" {
+	if ArgOutputDir == "" {
 		AnimaKit.PreviewWindow()
 	} else {
-		AnimaKit.RenderTo(ArgsOutput, ArgsNWorkers)
+		// Render frames
+		AnimaKit.RenderTo(ArgOutputDir, ArgNWorkers)
+
+		// Use ffmpeg to convert it to a video file if the user requested such a thing
+		if ArgOutputFile != "" {
+			cmd := exec.Command(
+				ArgFfmpeg,
+				"-y",
+				"-framerate",
+				fmt.Sprintf("%f", AnimaKit.TheAnimation.FPS),
+				"-i",
+				ArgOutputDir+"/%05d.png",
+				ArgOutputFile)
+			cmd.Env = append(os.Environ(),
+				"AV_LOG_FORCE_COLOR=true",
+			)
+			fmt.Println("Running:", cmd.Args)
+			ffmpeg_out, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("%s\n", ffmpeg_out)
+		}
 	}
 }
