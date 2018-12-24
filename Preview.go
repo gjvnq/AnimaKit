4e01383a063b3a0173700a175639321c33c4aa1a
Gabriel Queiroz <gabrieljvnq@gmail.com>
2018-12-24 02:27:35 -0200

    Basic preview working.

--- Preview.go
+++ Preview.go
@@ -17,6 +17,8 @@ func PreviewWindow() {
 	current_frame := 0
 	need_to_redraw := true
 	running := true
+	speed := 0
+	playing_last_update := unixMillis()
 	for running {
 		for event_raw := sdl.PollEvent(); event_raw != nil; event_raw = sdl.PollEvent() {
 			switch event := event_raw.(type) {
@@ -24,24 +26,84 @@ func PreviewWindow() {
 				println("Quit")
 				running = false
 				break
-			case *sdl.WindowEvent:
-				if event.Type != sdl.WINDOWEVENT_MOVED {
+			case *sdl.KeyboardEvent:
+				fmt.Println("----------------------")
+				fmt.Println(sdl.GetKeyName(event.Keysym.Sym))
+				fmt.Println("mod", event.Keysym.Mod, event.Keysym.Mod&sdl.KMOD_SHIFT)
+				fmt.Println("speed/frame", speed, current_frame)
+				if event.Type == sdl.KEYUP && event.Keysym.Sym == sdl.K_LEFT {
 					need_to_redraw = true
+					speed = 0
+					// [SHIFT] + [←] = first frame (and pause)
+					if event.Keysym.Mod&sdl.KMOD_SHIFT != 0 {
+						fmt.Println("First frame")
+						current_frame = 0
+					}
+					// [←] = previous frame
+					if event.Keysym.Mod == sdl.KMOD_NONE {
+						fmt.Println("Previous frame")
+						current_frame--
+					}
 				}
+				if event.Type == sdl.KEYUP && event.Keysym.Sym == sdl.K_RIGHT {
+					need_to_redraw = true
+					speed = 0
+					// [SHIFT] + [→] = last frame (and pause)
+					if event.Keysym.Mod&sdl.KMOD_SHIFT != 0 {
+						fmt.Println("Last frame")
+						fmt.Println(TheAnimation.Length, TheAnimation.FPS)
+						current_frame = int(TheAnimation.Length * TheAnimation.FPS)
+					}
+					// [→] = next frame
+					if event.Keysym.Mod == sdl.KMOD_NONE {
+						fmt.Println("Next frame")
+						current_frame++
+					}
+				}
+				// [SPACE] pauses and unpauses the animation
+				if event.Type == sdl.KEYUP && event.Keysym.Sym == sdl.K_SPACE {
+					if event.Keysym.Mod == sdl.KMOD_NONE {
+						if speed > 0 {
+							speed = 0
+							fmt.Println("stop playing")
+						} else {
+							speed = 1
+							fmt.Println("fwd")
+						}
+						playing_last_update = unixMillis()
+						// Shift makes it play in reverse
+					} else if event.Keysym.Mod&sdl.KMOD_SHIFT != 0 {
+						if speed < 0 {
+							speed = 0
+							fmt.Println("stop playing")
+						} else {
+							speed = -1
+							fmt.Println("rev")
+						}
+					}
+				}
+				fmt.Println(speed, current_frame)
 			}
 		}
+		if speed != 0 && unixMillis()-playing_last_update >= 1000/TheAnimation.FPS {
+			current_frame += speed
+			need_to_redraw = true
+			playing_last_update = unixMillis()
+		}
 		if need_to_redraw {
+			if current_frame < 0 {
+				current_frame = 0
+				speed = 0
+			}
+			fmt.Println(current_frame)
 			surface, err := window.GetSurface()
 			if err != nil {
 				panic(err)
 			}
 
 			need_to_redraw = false
-			fmt.Println(surface.Pitch)
-			fmt.Println(surface.Format)
 			TheAnimation.DrawOn(current_frame, surface)
 			window.UpdateSurface()
-			current_frame++
 		}
 	}
 }
